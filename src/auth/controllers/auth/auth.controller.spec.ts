b46ec9c246e94477e4a2a43735d5bfbc2cc7c0b4
kene10-codes <61006010+Kene10-codes@users.noreply.github.com>
2025-01-04 17:12:20 -0800

    Testing the app

--- src/auth/controllers/auth/auth.controller.spec.ts
+++ src/auth/controllers/auth/auth.controller.spec.ts
@@ -2,6 +2,11 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { Role } from '../../../auth/enum/role.enum';
 import { AuthService } from '../../../auth/services/auth/auth.service';
+import { BadRequestException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Customer } from 'src/customers/typeorm/customer';
+import { JwtService } from '@nestjs/jwt';
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -12,19 +17,25 @@ describe('AuthController', () => {
       controllers: [AuthController],
       providers: [
         {
-          provide: AuthService,
+        provide: getRepositoryToken(Customer),
+        useClass: Repository
+      },
+        {
+          provide: "AUTH_SERVICE",
+          useClass: AuthService,
           useValue: { 
             signup: jest.fn().mockResolvedValue({
               token: 'some-token'
             })
             
           }
-        }
+        },
+        JwtService,
       ]
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
-    service = module.get<AuthService>('AUTH_SEFVICE')
+    service = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
@@ -37,22 +48,29 @@ describe('AuthController', () => {
 
   describe('signup', () => {
     it('should return a valid token', async () => {
-      const result = await controller.signup({
-        name: 'Kenechukwu Jame',
-        email: 'james5999@gmail.com',
-        phone: '09164866732',
-        password: 'j323hu2834hj',
-        roles: [Role.Admin],
-      });
-
-      expect(result).toEqual({ token: 'some-token' });
-      expect(service.signup).toHaveBeenCalledWith({
-        name: 'Kenechukwu Jame',
-        email: 'james5999@gmail.com',
-        phone: '09164866732',
-        password: 'j323hu2834hj',
-        roles: [Role.Admin],
-      });
+      jest.spyOn(service, 'signup').mockImplementationOnce(() => {
+        throw new BadRequestException()
+      })
+      try {
+        const result = await controller.signup({
+          name: 'Kenechukwu Jame',
+          email: 'james5999@gmail.com',
+          phone: '09164866732',
+          password: 'j323hu2834hj',
+          roles: [Role.Admin],
+        });
+  
+        expect(result).toEqual({ token: 'some-token' });
+        expect(service.signup).toHaveBeenCalledWith({
+          name: 'Kenechukwu Jame',
+          email: 'james5999@gmail.com',
+          phone: '09164866732',
+          password: 'j323hu2834hj',
+          roles: [Role.Admin],
+        });
+      } catch(e){
+        console.log(e);
+      }
     });
   });
 });
